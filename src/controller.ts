0aaec157c43a7f8041c11b795c4ccaf054e390ef
Sam Stephenson <sam@37signals.com>
2016-10-04 18:20:28 -0500

    Can’t programmatically extend ES6 classes

--- src/controller.ts
+++ src/controller.ts
@@ -10,22 +10,27 @@ export interface Controller {
   disconnect()
 }
 
-export class DefaultController implements Controller {
-  element: Element
+export function DefaultController(element: Element) {
+  this.element = element
+  this.initialize()
+}
 
-  constructor(element: Element) {
-    this.element = element
-  }
-
-  connect() {}
-  disconnect() {}
+DefaultController.prototype = {
+  initialize: function() {},
+  connect: function() {},
+  disconnect: function() {}
 }
 
 export function controllerConstructorForPrototype(prototype): ControllerConstructor {
   const constructor = function(element: Element) {
     DefaultController.call(this, element)
   }
-  constructor.prototype = Object.create(prototype)
+  constructor.prototype = Object.create(DefaultController.prototype)
+
+  for (const key in prototype) {
+    const descriptor = Object.getOwnPropertyDescriptor(prototype, key)
+    Object.defineProperty(constructor.prototype, key, descriptor)
+  }
 
   const descriptor = { enumerable: false, writable: true, value: constructor }
   Object.defineProperty(constructor.prototype, "constructor", descriptor)
