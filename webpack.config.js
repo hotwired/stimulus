f8c5ebb952ee470ad3430543539b7c54dfad32fb
Javan Makhmali <javan@javan.us>
2017-03-03 09:58:16 -0800

    Configure webpack for different environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path')
 const webpack = require('webpack')
+const merge = require('webpack-merge')
 
-module.exports = {
+const config = {}
+
+config.all = {
   entry: {
     stimulus: './src/stimulus/index.ts',
   },
+
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, './dist'),
-    library: 'Stimulus'
+    library: 'Stimulus',
+    libraryTarget: 'umd'
   },
+
   module: {
     rules: [
       {
@@ -20,11 +26,33 @@ module.exports = {
       }
     ]
   },
+
   resolve: {
     extensions: ['.ts', '.js']
-  },
+  }
+}
+
+config.development = {
+  devtool: 'inline-source-map',
+
   devServer: {
     contentBase: path.join(__dirname, 'examples'),
     port: 9000
+  }
+}
+
+config.production = {
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
+
+module.exports = function(env = {}) {
+  const configEnv = Object.keys(env)[0]
+
+  if (configEnv) {
+    return merge(config.all, config[configEnv])
+  } else {
+    return config.all
   }
 }
