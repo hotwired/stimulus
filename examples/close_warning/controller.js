ac4f71b5a82c780ac6ce58bed61eab6d73d132e1
Sam Stephenson <sam@37signals.com>
2017-03-10 08:06:01 -0600

    Close warning example no longer needs to manually remove its action

--- examples/close_warning/controller.js
+++ examples/close_warning/controller.js
@@ -4,16 +4,10 @@ const { Descriptor, Action } = Stimulus
 app.register("close-warning", class extends Stimulus.Controller {
   initialize() {
     console.log("close-warning#initialize", this.identifier, this.element)
-  }
 
-  connect() {
     const descriptor = new Descriptor("beforeunload", "warn", true)
-    this.warnAction = new Action(this, window, window, descriptor)
-    this.addAction(this.warnAction)
-  }
-
-  disconnect() {
-    this.removeAction(this.warnAction)
+    const warnAction = new Action(this, window, window, descriptor)
+    this.addAction(warnAction)
   }
 
   warn(event) {
