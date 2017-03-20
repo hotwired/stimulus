f45c145c1121c803d32da47db57714e6fb8c0e8e
Javan Makhmali <javan@javan.us>
2017-03-20 17:58:43 -0400

    Add lo-fi autosave to close warning example

--- examples/controllers/close-warning-controller.js
+++ examples/controllers/close-warning-controller.js
@@ -7,9 +7,19 @@ export default class extends Controller {
 
     this.addAction("beforeunload->warn", window)
     this.addAction("click->focusInput")
+    this.addAction("input->autosave", "input")
     this.addAction("DOMFocusIn->clearWarning", "input")
     this.addAction("DOMFocusOut->addWarning", "input")
   }
+
+  connect() {
+    if (this.autosavedValue && this.autosavedValue != this.inputValue) {
+      this.showAutosaveDialog()
+    }
+    this.focusInput()
+  }
+
+  // Action methods
 
   warn(event) {
     if (this.hasUnsavedContent) {
@@ -35,6 +45,22 @@ export default class extends Controller {
     this.inputElement.classList.remove("warning")
   }
 
+  autosave() {
+    this.autosavedValue = this.inputValue
+  }
+
+  restoreFromAutosave() {
+    this.inputValue = this.autosavedValue
+    this.discardAutosave()
+  }
+
+  discardAutosave() {
+    localStorage.removeItem(this.autosaveKey)
+    this.hideAutosaveDialog()
+  }
+
+  // Input
+
   get hasUnsavedContent() {
     return this.inputValue.length > 0
   }
@@ -43,7 +69,37 @@ export default class extends Controller {
     return this.inputElement.value
   }
 
+  set inputValue(value) {
+    this.inputElement.value = value
+  }
+
   get inputElement() {
     return this.targets.find("input")
+  }
+
+  // Autosave
+
+  showAutosaveDialog() {
+    this.autosaveElement.classList.remove("hidden")
+  }
+
+  hideAutosaveDialog() {
+    this.autosaveElement.classList.add("hidden")
+  }
+
+  get autosaveElement() {
+    return this.targets.find("autosave")
+  }
+
+  get autosaveKey() {
+    return "close-warning:autosave"
+  }
+
+  get autosavedValue() {
+    return localStorage.getItem(this.autosaveKey)
+  }
+
+  set autosavedValue(value) {
+    localStorage.setItem(this.autosaveKey, value)
   }
 }
