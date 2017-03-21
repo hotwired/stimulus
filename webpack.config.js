3dfb921fb8c17ff00a3c687f9e753dbade137c14
Javan Makhmali <javan@javan.us>
2017-03-21 14:12:44 -0400

    Pass examples through babel for decorator support

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,26 @@ config.all = {
             }
           }
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/
+        ],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ["es2015", { modules: false }]
+              ],
+              plugins: [
+                "syntax-dynamic-import",
+                "transform-decorators-legacy"
+              ]
+            }
+          }
+        ]
       }
     ]
   },
