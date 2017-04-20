7ee029ded140bed4f0a74fe02f5c6dd0c68fd5e7
Javan Makhmali <javan@javan.us>
2017-04-20 16:59:11 -0400

    Use element's dataset, add #toJSON

--- src/stimulus/data_set.ts
+++ src/stimulus/data_set.ts
@@ -1,33 +1,51 @@
 export class DataSet {
   identifier: string
-  element: Element
+  keyPattern: RegExp
+  stringMap: DOMStringMap
 
   constructor(identifier: string, element: Element) {
     this.identifier = identifier
-    this.element = element
+    this.keyPattern = new RegExp(`^${identifier}([A-Z]+.*)`)
+    this.stringMap = (element as HTMLElement).dataset
   }
 
-  get(key: string): string | null {
-    const name = this.getAttributeNameForKey(key)
-    return this.element.getAttribute(name)
+  get(key: string): string | undefined {
+    key = this.getFormattedKey(key)
+    return this.stringMap[key]
   }
 
-  set(key: string, value): void {
-    const name = this.getAttributeNameForKey(key)
-    this.element.setAttribute(name, value)
+  set(key: string, value) {
+    key = this.getFormattedKey(key)
+    return this.stringMap[key] = value
   }
 
-  private getAttributeNameForKey(key: string): string {
-    return `data-${this.identifier}-${dasherize(key)}`
+  toJSON() {
+    const result = {}
+    for (const key in this.stringMap) {
+      const unformattedKey = this.getUnformattedKey(key)
+      if (unformattedKey) {
+        result[unformattedKey] = this.stringMap[key]
+      }
+    }
+    return result
+  }
+
+  private getFormattedKey(key: string): string {
+    return `${this.identifier}${capitalize(key)}`
+  }
+
+  private getUnformattedKey(key: string): string | void {
+    const matches = key.match(this.keyPattern)
+    if (matches) {
+      return uncapitalize(matches[1])
+    }
   }
 }
 
-function dasherize(camelString) {
-  if (/[A-Z]/.test(camelString)) {
-    const element = document.createElement("span")
-    element.dataset[camelString] = ""
-    return element.attributes[0].name.slice(5)
-  } else {
-    return camelString
-  }
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function uncapitalize(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1)
 }
