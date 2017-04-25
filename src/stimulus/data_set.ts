b56a2b3a493bd36313ba1a0200abe2e617268100
Sam Stephenson <sam@37signals.com>
2017-04-25 12:02:18 -0700

    Remove DataSet#toJSON

--- src/stimulus/data_set.ts
+++ src/stimulus/data_set.ts
@@ -1,11 +1,9 @@
 export class DataSet {
   identifier: string
-  keyPattern: RegExp
   stringMap: DOMStringMap
 
   constructor(identifier: string, element: Element) {
     this.identifier = identifier
-    this.keyPattern = new RegExp(`^${identifier}([A-Z]+.*)`)
     this.stringMap = (element as HTMLElement).dataset
   }
 
@@ -19,33 +17,11 @@ export class DataSet {
     return this.stringMap[key] = value
   }
 
-  toJSON() {
-    const result = {}
-    for (const key in this.stringMap) {
-      const unformattedKey = this.getUnformattedKey(key)
-      if (unformattedKey) {
-        result[unformattedKey] = this.stringMap[key]
-      }
-    }
-    return result
-  }
-
   private getFormattedKey(key: string): string {
     return `${this.identifier}${capitalize(key)}`
-  }
-
-  private getUnformattedKey(key: string): string | void {
-    const matches = key.match(this.keyPattern)
-    if (matches) {
-      return uncapitalize(matches[1])
-    }
   }
 }
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
-}
-
-function uncapitalize(string) {
-  return string.charAt(0).toLowerCase() + string.slice(1)
 }
