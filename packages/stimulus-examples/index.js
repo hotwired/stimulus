49a332eb62602cdf02c81f3aeaf127f2da7db73b
Javan Makhmali <javan@javan.us>
2017-10-24 18:42:59 -0400

    Add automatic controller registration support via webpack's `require.context`

--- packages/stimulus-examples/index.js
+++ packages/stimulus-examples/index.js
@@ -1,18 +1,12 @@
 import "stimulus-polyfills"
 import { Application, LogLevel } from "stimulus"
+import { autoload } from "stimulus-webpack"
 import Turbolinks from "turbolinks"
 
 const application = new Application({ logLevel: LogLevel.DEBUG })
-const requireContext = require.context("./controllers", true, /\.js$/)
 
-requireContext.keys().forEach((key) => {
-  const matches = key.match(/\/(.+)-controller/)
-  if (matches) {
-    const identifier = matches[1]
-    const controllerConstructor = requireContext(key).default
-    application.register(identifier, controllerConstructor)
-  }
-})
+const context = require.context("./controllers", true, /\.js$/)
+autoload(context, application)
 
 application.start()
 Turbolinks.start()
