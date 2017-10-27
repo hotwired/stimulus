8907d1389307a2e40f19a9ebdab528b2e9ea29ce
Javan Makhmali <javan@javan.us>
2017-10-27 12:59:27 -0400

    Update package names

--- packages/@stimulus/examples/index.js
+++ packages/@stimulus/examples/index.js
@@ -1,6 +1,6 @@
-import "stimulus-polyfills"
+import "@stimulus/polyfills"
 import { Application, LogLevel } from "stimulus"
-import { autoload } from "stimulus-webpack"
+import { autoload } from "@stimulus/webpack-helpers"
 import Turbolinks from "turbolinks"
 
 const application = new Application({ logLevel: LogLevel.DEBUG })
