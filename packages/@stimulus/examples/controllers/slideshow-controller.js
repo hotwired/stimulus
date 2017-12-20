51642222a7cac78d68223ae686bea447f6e491d3
Javan Makhmali <javan@javan.us>
2017-12-20 17:46:54 -0500

    Replace wizard example with slideshow example

---
+++ packages/@stimulus/examples/controllers/slideshow-controller.js
@@ -0,0 +1,47 @@
+import { Controller } from "stimulus"
+
+export default class SlideshowController extends Controller {
+  connect() {
+    this.showSlideAtIndex(this.currentIndex)
+  }
+
+  next() {
+    this.showSlideAtIndex(this.nextIndex)
+  }
+
+  previous() {
+    this.showSlideAtIndex(this.previousIndex)
+  }
+
+  showSlideAtIndex(index) {
+    this.currentSlideElement.classList.remove("slide--current")
+    this.data.set("index", index)
+    this.currentSlideElement.classList.add("slide--current")
+  }
+
+  get currentIndex() {
+    if (this.data.has("index")) {
+      return Number(this.data.get("index"))
+    } else {
+      return 0
+    }
+  }
+
+  get nextIndex() {
+    const index = this.currentIndex + 1
+    return this.slideElements[index] ? index : 0
+  }
+
+  get previousIndex() {
+    const index = this.currentIndex - 1
+    return this.slideElements[index] ? index : this.slideElements.length - 1
+  }
+
+  get currentSlideElement() {
+    return this.slideElements[this.currentIndex]
+  }
+
+  get slideElements() {
+    return this.targets.findAll("slide")
+  }
+}
