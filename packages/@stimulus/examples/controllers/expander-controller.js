4eb1b287e490908bd722d7acdbba5fcf7e6c7ecb
Javan Makhmali <javan@javan.us>
2017-12-20 16:43:58 -0500

    Rework the expander example using the data API

--- packages/@stimulus/examples/controllers/expander-controller.js
+++ packages/@stimulus/examples/controllers/expander-controller.js
@@ -1,25 +1,19 @@
 import { Controller } from "stimulus"
 
 export default class ExpanderController extends Controller {
-  expand(event) {
-    this.allTargets.forEach(e => e.classList.add("expanded"))
-  }
-
-  collapse(event) {
-    this.allTargets.forEach(e => e.classList.remove("expanded"))
-  }
-
-  toggle(event) {
+  toggle() {
     this.isExpanded ? this.collapse() : this.expand()
   }
 
-  get isExpanded() {
-    return this.element.classList.contains("expanded")
+  expand() {
+    this.data.set("expanded", true)
   }
 
-  get allTargets() {
-    const collapseTargets = this.targets.findAll("collapsed")
-    const expandTargets = this.targets.findAll("expanded")
-    return [this.element].concat(collapseTargets).concat(expandTargets)
+  collapse() {
+    this.data.delete("expanded")
+  }
+
+  get isExpanded() {
+    return this.data.has("expanded")
   }
 }
