853876b67c0892be7a19c5086d8cc5dc43e25a0a
Sam Stephenson <sam@37signals.com>
2017-12-20 15:17:47 -0600

    Remove CSS.escape
    Safari 9 trips up the polyfill: https://github.com/mathiasbynens/CSS.escape/issues/2

--- packages/@stimulus/core/src/selectors.ts
+++ packages/@stimulus/core/src/selectors.ts
@@ -1,7 +1,3 @@
-declare var CSS: {
-  escape(string: string): string
-}
-
 export function attributeValueContainsToken(attributeName: string, token: string) {
-  return `[${CSS.escape(attributeName)}~="${CSS.escape(token)}"]`
+  return `[${attributeName}~="${token}"]`
 }
