018b7696ae79b1b58fbbd1c5aedf15a73a6e6088
Sam Stephenson <sam@37signals.com>
2018-01-12 14:38:15 -0600

    Use @types/webpack-env

--- packages/@stimulus/webpack-helpers/index.ts
+++ packages/@stimulus/webpack-helpers/index.ts
@@ -1,32 +1,24 @@
 import { Definition } from "@stimulus/core"
 
-// https://webpack.js.org/guides/dependency-management/#require-context
-export interface Context {
-  (key: string): Module
-  keys(): Array<string>
-  resolve(key: string): number
-  id: number
-}
-
-export interface Module {
+export interface ECMAScriptModule {
   __esModule: boolean
   default?: object
 }
 
-export function definitionsFromContext(context: Context): Definition[] {
+export function definitionsFromContext(context: __WebpackModuleApi.RequireContext): Definition[] {
   return context.keys()
     .map(key => definitionForModuleWithContextAndKey(context, key))
     .filter(value => value) as Definition[]
 }
 
-function definitionForModuleWithContextAndKey(context: Context, key: string): Definition | undefined {
+function definitionForModuleWithContextAndKey(context: __WebpackModuleApi.RequireContext, key: string): Definition | undefined {
   const identifier = identifierForContextKey(key)
   if (identifier) {
     return definitionForModuleAndIdentifier(context(key), identifier)
   }
 }
 
-function definitionForModuleAndIdentifier(module: Module, identifier: string): Definition | undefined {
+function definitionForModuleAndIdentifier(module: ECMAScriptModule, identifier: string): Definition | undefined {
   const controllerConstructor = module.default
   if (typeof controllerConstructor == "function") {
     return { identifier, controllerConstructor }
