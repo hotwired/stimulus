174cf6f6b83612a2719dd2cee91dc221c8ac87f7
Javan Makhmali <javan@javan.us>
2018-08-15 07:58:59 -0400

    CI: Freshen browser list
    - Update to latest desktop browser versions
    - Add iOS and Android

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -42,15 +42,15 @@ const config = {
 
 if (process.env.CI) {
   config.customLaunchers = {
-    sl_chrome: {
+    sl_chrome_latest: {
       base: "SauceLabs",
       browserName: "chrome",
-      version: "63"
+      version: "68"
     },
-    sl_chrome_lang_tl: {
+    sl_chrome_latest_i8n: {
       base: "SauceLabs",
       browserName: "chrome",
-      version: "63",
+      version: "68",
       chromeOptions: {
         args: ["--lang=tr"]
       }
@@ -60,10 +60,10 @@ if (process.env.CI) {
       browserName: "firefox",
       version: "43"
     },
-    sl_firefox: {
+    sl_firefox_latest: {
       base: "SauceLabs",
       browserName: "firefox",
-      version: "57"
+      version: "61"
     },
     sl_safari_9: {
       base: "SauceLabs",
@@ -77,23 +77,37 @@ if (process.env.CI) {
       platform: "macOS 10.12",
       version: "10.1"
     },
-    sl_safari_11: {
+    sl_safari_latest: {
       base: "SauceLabs",
       browserName: "safari",
       platform: "macOS 10.13",
-      version: "11"
+      version: "11.1"
     },
-    sl_edge: {
+    sl_edge_latest: {
       base: "SauceLabs",
       browserName: "microsoftedge",
       platform: "Windows 10",
-      version: "15.15063"
+      version: "17.17134"
     },
-    sl_ie: {
+    sl_ie_11: {
       base: "SauceLabs",
       browserName: "internet explorer",
       platform: "Windows 8.1",
       version: "11"
+    },
+    sl_ios_latest: {
+      base: "SauceLabs",
+      browserName: "safari",
+      platform: "ios",
+      device: "iPhone X Simulator",
+      version: "11.3"
+    },
+    sl_android_latest: {
+      base: "SauceLabs",
+      browserName: "chrome",
+      platform: "android",
+      device: "Android GoogleAPI Emulator",
+      version: "7.1"
     }
   }
   config.browsers = Object.keys(config.customLaunchers)
