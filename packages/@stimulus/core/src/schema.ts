6ca14e32935aa7133039eacf6ec2a5d6a122c6ac
Sam Stephenson <sam@hey.com>
2020-12-04 11:25:05 -0500

    Remove defunct Schema#classAttribute

--- packages/@stimulus/core/src/schema.ts
+++ packages/@stimulus/core/src/schema.ts
@@ -2,12 +2,10 @@ export interface Schema {
   controllerAttribute: string
   actionAttribute: string
   targetAttribute: string
-  classAttribute: string
 }
 
 export const defaultSchema: Schema = {
   controllerAttribute: "data-controller",
   actionAttribute: "data-action",
-  targetAttribute: "data-target",
-  classAttribute: "data-class"
+  targetAttribute: "data-target"
 }
