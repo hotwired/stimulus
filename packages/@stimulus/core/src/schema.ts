0b035b0b762220c61030b4cb09b4743a1298e9a1
Josh Goodall <inopinatus@inopinatus.org>
2021-01-04 16:20:55 +1100

    Target attribute name is part of the schema.

--- packages/@stimulus/core/src/schema.ts
+++ packages/@stimulus/core/src/schema.ts
@@ -2,10 +2,12 @@ export interface Schema {
   controllerAttribute: string
   actionAttribute: string
   targetAttribute: string
+  targetAttributeForScope(identifier: string): string
 }
 
 export const defaultSchema: Schema = {
   controllerAttribute: "data-controller",
   actionAttribute: "data-action",
-  targetAttribute: "data-target"
+  targetAttribute: "data-target",
+  targetAttributeForScope: identifier => `data-${identifier}-target`
 }
