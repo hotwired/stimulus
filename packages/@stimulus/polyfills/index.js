d0d70ce7a124d484647dbebd98f04f5d47c6d6ea
Marco Roth <marco.roth@intergga.ch>
2021-06-20 18:20:27 +0200

    add `Array.includes()` polyfill

--- packages/@stimulus/polyfills/index.js
+++ packages/@stimulus/polyfills/index.js
@@ -1,6 +1,7 @@
 import "core-js/es/array/find"
 import "core-js/es/array/find-index"
 import "core-js/es/array/from"
+import "core-js/es/array/includes"
 import "core-js/es/map"
 import "core-js/es/object/assign"
 import "core-js/es/promise"
