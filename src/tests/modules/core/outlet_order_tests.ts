15a3675c915c33fb8158cd4ff3ea365576e9e06f
Marco Roth <marco.roth@intergga.ch>
2023-06-18 23:34:12 +0200

    Also test connect order

--- src/tests/modules/core/outlet_order_tests.ts
+++ src/tests/modules/core/outlet_order_tests.ts
@@ -1,12 +1,22 @@
 import { ControllerTestCase } from "../../cases/controller_test_case"
 import { OutletController } from "../../controllers/outlet_controller"
 
-export default class OutletOrderTests extends ControllerTestCase(OutletController) {
+const connectOrder: string[] = []
+
+class OutletOrderController extends OutletController {
+  connect() {
+    connectOrder.push(`${this.identifier}-${this.element.id}-start`)
+    super.connect()
+    connectOrder.push(`${this.identifier}-${this.element.id}-end`)
+  }
+}
+
+export default class OutletOrderTests extends ControllerTestCase(OutletOrderController) {
   fixtureHTML = `
-    <div data-controller="alpha" data-alpha-beta-outlet=".beta">Search</div>
-    <div data-controller="beta" class="beta">Beta</div>
-    <div data-controller="beta" class="beta">Beta</div>
-    <div data-controller="beta" class="beta">Beta</div>
+    <div data-controller="alpha" id="alpha1" data-alpha-beta-outlet=".beta">Search</div>
+    <div data-controller="beta" id="beta-1" class="beta">Beta</div>
+    <div data-controller="beta" id="beta-2" class="beta">Beta</div>
+    <div data-controller="beta" id="beta-3" class="beta">Beta</div>
   `
 
   get identifiers() {
@@ -20,5 +30,16 @@ export default class OutletOrderTests extends ControllerTestCase(OutletControlle
       this.assert.equal(outlet.identifier, "beta")
       this.assert.equal(Array.from(outlet.element.classList.values()), "beta")
     })
+
+    this.assert.deepEqual(connectOrder, [
+      "alpha-alpha1-start",
+      "beta-beta-1-start",
+      "beta-beta-1-end",
+      "beta-beta-2-start",
+      "beta-beta-2-end",
+      "beta-beta-3-start",
+      "beta-beta-3-end",
+      "alpha-alpha1-end",
+    ])
   }
 }
